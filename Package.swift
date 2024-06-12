d2f9ea8abef5c3c3d91a46c3e929fa247ce2cce0
Jack Palevich <jack.palevich@gmail.com>
2024-06-11 20:37:21 -0700

    Update to require MacOS 15.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "ToyPathTracerSwift",
-    platforms: [.macOS(.v10_15), .iOS(.v18)],
+    platforms: [.macOS(.v15), .iOS(.v18)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
